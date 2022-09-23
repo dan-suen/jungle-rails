9a0aa3e5623f7ec38e5d635ae8eb63200776ae83
Daniel Suen <danielsuen2007@yahoo.ca>
2022-09-22 23:50:22 -0400

    implemented counts on admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => "Jungle", :password => "book"
   def show
+    @products_count = Product.all.length
+    @categories_count = Category.all.length
   end
 end
