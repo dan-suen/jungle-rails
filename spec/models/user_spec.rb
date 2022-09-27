07e96c2f8e08744976e5986b5ee1a9b7f3e7228f
Daniel Suen <danielsuen2007@yahoo.ca>
2022-09-27 15:23:44 -0400

    added last name field to user table and implemented changes to views and controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "validates email field" do
+      @user = User.create({
+        name:  'Lion Grapevine',
+        password: "password",
+        email: nil
+      })
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "validates name fields" do
+      @user = User.create({
+        name: nil,
+        password: "password",
+        email: "apple@com"
+      })
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
